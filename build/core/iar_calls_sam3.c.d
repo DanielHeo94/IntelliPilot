6cda8fbfc4d9e289a796879b1ee8d36dc450f9ee
Daniel Heo <peterjinsoo@gmail.com>
2016-08-17 04:19:56 +0900

    *.d files deleted

--- build/core/iar_calls_sam3.c.d
+++
@@ -1,2 +0,0 @@
-/home/daniel/workspace/IntelliPilot/build/core/iar_calls_sam3.c.o: \
- /home/daniel/workspace/IntelliPilot/.arduino15/packages/arduino/hardware/sam/1.6.9/cores/arduino/iar_calls_sam3.c
